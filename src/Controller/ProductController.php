a2653a6d928e34ed1619826112e657778021f6e1
pierre gosselin <pierre.gosselin@orange.fr>
2019-08-21 12:42:56 +0200

    show product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -14,23 +14,57 @@ class ProductController extends AbstractController
     {
         // On intialise un tableau avec des produits
         // L'attribut $products est accessible sur toutes les routes...
-        $this->products = [['Nom'=>'iPhone X','Slug'=>'iphone-x','Description'=>'Un iPhone de 2017','Prix'=>'999'],
-                           ['Nom'=>'iPhone XR','Slug'=>'iphone-xr','Description'=>'Un iPhone de 2018','Prix'=>'1099'],
-                           ['Nom'=>'iPhone XS','Slug'=>'iphone-xs','Description'=>'Un iPhone de 2019','Prix'=>'1999']
+        $this->products = [['name'=>'iPhone X','slug'=>'iphone-x','description'=>'Un iPhone de 2017','price'=>'999'],
+                           ['name'=>'iPhone XR','slug'=>'iphone-xr','description'=>'Un iPhone de 2018','price'=>'1099'],
+                           ['name'=>'iPhone XS','slug'=>'iphone-xs','description'=>'Un iPhone de 2019','price'=>'1999']
         ];
     }
 
     /**
-     * @Route("/random",name="random")
+     * @Route("/product/random",name="random")
      */
     public function random()
     {
-        $i = rand(0,count($this->products)-1);
-        $show = "";
-        foreach ($this->products[$i] as $product)
+        // On récupère une clé aléatoire du tableau
+        $key = array_rand($this->products);
+        // On récupère le product "random"
+        $product = $this->products[$key];
+
+        return $this->render('product/random.html.twig',['product' => $product]);
+    }
+
+    /**
+     * @Route("/product",name="product")
+     */
+    public function product()
+    {
+        return $this->render('product/show.html.twig',['products' => $this->products]);
+    }
+
+    /**
+     * @Route("/product/{slug}",name="oneProduct")
+     */
+    public function oneProduct($slug)
+    {
+        // On va parcourir tous les produits
+        foreach ($this->products as $product)
         {
-            $show .= " / " .$product;
+            // Si le slug du produit correspond à celui de l'URL
+            if ($product['slug'] === $slug)
+            {
+                // Si un produit existe avec le slug, on retourne le template et on arrête le code
+                return $this->render('product/random.html.twig',['product' => $product]);
+            }
         }
-        return new Response('<body>Produit aléatoire :'.$show.' </body>');
+        // On s'assure de parcourir tout le tableau et seulement on affiche la 404
+        throw $this->createNotFoundException();
+    }
+
+    /**
+     * @Route("/product/create",name="create")
+     */
+    public function create()
+    {
+
     }
 }
